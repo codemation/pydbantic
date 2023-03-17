b37fc0783b109a62e5122b484b6b7f1a6019a92e
Joshua (codemation) <joshua.jamison@nostos-genomics.com>
2023-03-17 12:09:48 +0100

    formatting

--- pydbantic/translations.py
+++ pydbantic/translations.py
@@ -2,135 +2,38 @@ import sqlalchemy
 
 DEFAULT_TRANSLATIONS = {
     "MYSQL": {
-        str: {
-            "column_type": sqlalchemy.String,
-            "args": [60],
-            "kwargs": {}
-        },
-        int: {
-            "column_type": sqlalchemy.Integer,
-            "args": [],
-            "kwargs": {}
-        },
-        float: {
-            "column_type": sqlalchemy.Float,
-            "args": [],
-            "kwargs": {}
-        },
-        bool: {
-            "column_type": sqlalchemy.Boolean,
-            "args": [],
-            "kwargs": {}
-        },
-        dict: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        list: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [1024],
-            "kwargs": {}
-        },
-        tuple: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
+        str: {"column_type": sqlalchemy.String, "args": [60], "kwargs": {}},
+        int: {"column_type": sqlalchemy.Integer, "args": [], "kwargs": {}},
+        float: {"column_type": sqlalchemy.Float, "args": [], "kwargs": {}},
+        bool: {"column_type": sqlalchemy.Boolean, "args": [], "kwargs": {}},
+        dict: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        list: {"column_type": sqlalchemy.LargeBinary, "args": [1024], "kwargs": {}},
+        tuple: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
         "default_primary": {
             "column_type": sqlalchemy.BINARY,
             "args": [255],
-            "kwargs": {}
+            "kwargs": {},
         },
-        "default": {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        }
+        "default": {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
     },
     "POSTGRES": {
-        int: {
-            "column_type": sqlalchemy.Integer,
-            "args": [],
-            "kwargs": {}
-        },
-        float: {
-            "column_type": sqlalchemy.Float,
-            "args": [],
-            "kwargs": {}
-        },
-        bool: {
-            "column_type": sqlalchemy.Boolean,
-            "args": [],
-            "kwargs": {}
-        },
-        dict: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        list: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        tuple: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        "default": {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-
-        str: {
-            "column_type": sqlalchemy.String,
-            "args": [],
-            "kwargs": {}
-        },
+        int: {"column_type": sqlalchemy.Integer, "args": [], "kwargs": {}},
+        float: {"column_type": sqlalchemy.Float, "args": [], "kwargs": {}},
+        bool: {"column_type": sqlalchemy.Boolean, "args": [], "kwargs": {}},
+        dict: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        list: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        tuple: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        "default": {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        str: {"column_type": sqlalchemy.String, "args": [], "kwargs": {}},
     },
     "SQLITE": {
-        str: {
-            "column_type": sqlalchemy.String,
-            "args": [60],
-            "kwargs": {}
-        },
-        int: {
-            "column_type": sqlalchemy.Integer,
-            "args": [],
-            "kwargs": {}
-        },
-        float: {
-            "column_type": sqlalchemy.Float,
-            "args": [],
-            "kwargs": {}
-        },
-        bool: {
-            "column_type": sqlalchemy.Boolean,
-            "args": [],
-            "kwargs": {}
-        },
-        dict: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        list: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        tuple: {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        },
-        "default": {
-            "column_type": sqlalchemy.LargeBinary,
-            "args": [],
-            "kwargs": {}
-        }
+        str: {"column_type": sqlalchemy.String, "args": [60], "kwargs": {}},
+        int: {"column_type": sqlalchemy.Integer, "args": [], "kwargs": {}},
+        float: {"column_type": sqlalchemy.Float, "args": [], "kwargs": {}},
+        bool: {"column_type": sqlalchemy.Boolean, "args": [], "kwargs": {}},
+        dict: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        list: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        tuple: {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
+        "default": {"column_type": sqlalchemy.LargeBinary, "args": [], "kwargs": {}},
     },
 }
