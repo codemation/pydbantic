3b1820bd4e5f93f40aeef4c2c3eab7182cac0182
Joshua (codemation) <joshua.jamison@nostos-genomics.com>
2023-04-16 00:02:15 +0200

    ensure rc is 0 when running test migrations in sequence

--- tests/migrations/test_migrations.py
+++ tests/migrations/test_migrations.py
@@ -1,4 +1,5 @@
 import pytest
 
 for i in range(0, 9):
-    pytest.main([f"tests/migrations/test_model_migration_{i}.py"])
+    result = pytest.main([f"tests/migrations/test_model_migration_{i}.py"])
+    assert result == 0
